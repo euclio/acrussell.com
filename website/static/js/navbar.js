d8f5be8ca10ec8af321e23494cebc192f76069da
Andy Russell <arussell123@gmail.com>
2013-07-23 23:28:42 -0700

    fix highlighting of navbar in blog

--- website/static/js/navbar.js
+++ website/static/js/navbar.js
@@ -1,5 +1,9 @@
-var url = window.location.href;
-url = '/' + url.substr(url.lastIndexOf('/') + 1);
+var url = window.location.pathname;
+var first_slash = url.indexOf('/', 1);
+
+var substring_length = first_slash > -1 ? first_slash : url.length;
+url = url.substring(0, substring_length);
+
 $(document).ready(function() {
-    $('nav').find('a[href="' + url + '"]').addClass('currentlink');
+    $('nav').find('a[href^="' + url + '"]').first().addClass('currentlink');
 });
