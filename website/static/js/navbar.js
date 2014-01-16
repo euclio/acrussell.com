23e69a75231a403c365122c166b8e8835f61c2be
Andy Russell <arussell123@gmail.com>
2014-01-16 12:44:26 -0800

    add .currentLink & .external to navbar server-side

--- website/static/js/navbar.js
+++
@@ -1,9 +0,0 @@
-var url = window.location.pathname;
-var first_slash = url.indexOf('/', 1);
-
-var substring_length = first_slash > -1 ? first_slash : url.length;
-url = url.substring(0, substring_length);
-
-$(document).ready(function() {
-    $('nav').find('a[href^="' + url + '"]').first().addClass('currentlink');
-});
