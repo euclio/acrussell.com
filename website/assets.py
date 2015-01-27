4e78ed2d76c9d9de592e32f26ba7d80db6048f15
Andy Russell <arussell123@gmail.com>
2015-01-26 17:18:11 -0800

    replace vendor prefixing with autoprefixer

--- website/assets.py
+++ website/assets.py
@@ -1,3 +1,5 @@
+import os
+
 from flask.ext.assets import Bundle, Environment
 
 css = Bundle(
@@ -6,9 +8,14 @@ css = Bundle(
     'css/projects.css',
     'css/resume.css',
     'css/slideshow.css',
+    filters='autoprefixer',
+    output='gen/main.css'
 )
 
 def init_app(app):
     assets = Environment(app)
+
+    assets.config['autoprefixer_bin'] = os.path.join(
+        app.config['APP_ROOT'], 'node_modules', 'autoprefixer', 'autoprefixer')
 
     assets.register('css', css)
