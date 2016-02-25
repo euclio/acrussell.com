6c01fa08838b38c2dfbc6361b2899e50119601af
Andy Russell <arussell123@gmail.com>
2016-02-25 16:10:59 -0500

    check output of commands in build script

--- build.rs
+++ build.rs
@@ -9,29 +9,33 @@ fn main() {
 
     // Compile SCSS
     let scss_out = &out_dir.join("styles.css");
-    Command::new("sass")
-        .arg("scss/main.scss")
-        .arg(scss_out.as_path().to_str().unwrap())
-        .spawn()
-        .unwrap_or_else(|e| {
-            match e.kind() {
-                ErrorKind::NotFound => panic!("sass not installed"),
-                _ => panic!(e),
-            }
-        });
+    let mut sass = Command::new("sass")
+                       .arg("scss/main.scss")
+                       .arg(scss_out.as_path().to_str().unwrap())
+                       .spawn()
+                       .unwrap_or_else(|e| {
+                           match e.kind() {
+                               ErrorKind::NotFound => panic!("sass not installed"),
+                               _ => panic!(e),
+                           }
+                       });
+    assert!(sass.wait().unwrap().success(),
+            "there was a problem compiling scss");
 
     // Apply PostCSS
     let output = &root.join("static/css/");
     fs::create_dir_all(&output).unwrap();
-    Command::new("postcss")
-        .args(&["--use", "autoprefixer"])
-        .arg(scss_out.as_path().to_str().unwrap())
-        .args(&["-d", output.as_path().to_str().unwrap()])
-        .spawn()
-        .unwrap_or_else(|e| {
-            match e.kind() {
-                ErrorKind::NotFound => panic!("postcss not installed"),
-                _ => panic!(e),
-            }
-        });
+    let mut postcss = Command::new("postcss")
+                          .args(&["--use", "autoprefixer"])
+                          .arg(scss_out.as_path().to_str().unwrap())
+                          .args(&["-d", output.as_path().to_str().unwrap()])
+                          .spawn()
+                          .unwrap_or_else(|e| {
+                              match e.kind() {
+                                  ErrorKind::NotFound => panic!("postcss not installed"),
+                                  _ => panic!(e),
+                              }
+                          });
+    assert!(postcss.wait().unwrap().success(),
+            "there was a problem postprocessing css");
 }
