3656fa9eadd2c3048ea2abf8e286dd7f4bdaa852
Andy Russell <arussell123@gmail.com>
2017-06-20 13:36:43 -0400

    refactor(build): use error-chain

--- build.rs
+++ build.rs
@@ -1,47 +1,60 @@
+#![recursion_limit = "1024"]
+
 use std::env;
 use std::fs;
-use std::io::ErrorKind;
 use std::path::PathBuf;
-use std::process::Command;
+use std::process::{Command, Stdio};
 
-fn main() {
+#[macro_use]
+extern crate error_chain;
+
+mod errors {
+    use std::io;
+
+    error_chain! {
+        foreign_links {
+            Io(io::Error);
+        }
+    }
+}
+
+use errors::*;
+
+fn run() -> Result<()> {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
 
-    // Copy static assets to target directory.
-    let copy_status = Command::new("cp")
-        .arg("-r")
-        .arg("static")
-        .arg(out_dir.to_str().unwrap())
+    let status = Command::new("cp")
+        .args(&["-r", "static", out_dir.to_str().unwrap()])
+        .stderr(Stdio::inherit())
         .status()
-        .unwrap();
-    assert!(copy_status.success());
+        .chain_err(|| "failed to run `cp`")?;
+    if !status.success() {
+        bail!("failed to copy static files to the target directory");
+    }
 
-    // Compile SCSS
-    let scss_out = &out_dir.join("styles.css");
-    let sass_status = Command::new("sass")
-        .arg("scss/main.scss")
-        .arg(scss_out.to_str().unwrap())
+    let scss_out = out_dir.join("styles.css");
+    let status = Command::new("sass")
+        .args(&["scss/main.scss", scss_out.to_str().unwrap()])
+        .stderr(Stdio::inherit())
         .status()
-        .unwrap_or_else(|e| match e.kind() {
-            ErrorKind::NotFound => panic!("sass not installed"),
-            _ => panic!(e),
-        });
-    assert!(sass_status.success(), "there was a problem compiling scss");
+        .chain_err(|| "failed to run `sass`")?;
+    if !status.success() {
+        bail!("SCSS compilation failed");
+    }
 
-    // Apply PostCSS
-    let output = &out_dir.join("static/css/");
-    fs::create_dir_all(&output).unwrap();
-    let postcss_status = Command::new("postcss")
+    let css_out = out_dir.join("static/css/");
+    fs::create_dir_all(&css_out)?;
+    let status = Command::new("postcss")
         .args(&["--use", "autoprefixer"])
         .arg(scss_out.to_str().unwrap())
-        .args(&["-d", output.to_str().unwrap()])
+        .args(&["-d", css_out.to_str().unwrap()])
         .status()
-        .unwrap_or_else(|e| match e.kind() {
-            ErrorKind::NotFound => panic!("postcss not installed"),
-            _ => panic!(e),
-        });
-    assert!(
-        postcss_status.success(),
-        "there was a problem postprocessing css"
-    );
+        .chain_err(|| "failed to run `postcss`")?;
+    if !status.success() {
+        bail!("failed postprocessing CSS");
+    }
+
+    Ok(())
 }
+
+quick_main!(run);
